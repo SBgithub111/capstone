49e09a4c68ac1aa10abb3526d48a161ef9c9b0cb
SBgithub111 <sbpublic111@gmail.com>
2021-12-04 17:56:12 +0900

    211204GPS
    기존 웹사이트 서버를 이용해서 우리가 구현하려 한 부분을 거의 완벽히 작동하게 만든 앱, 하지만 서버를 이용하기 때문에 이식하기 어렵고 활용 불가능

---
+++ flutter_ba:Users:seonghunbyeon:Downloads:flutter_background_geolocation-4.3.1ckground_geolocation-4.3.1/example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Volumes/Glyph2TB/Users/chris/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Volumes/Glyph2TB/Users/chris/workspace/background-geolocation/flutter/flutter_background_geolocation/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=4.0.0"
+export "FLUTTER_BUILD_NUMBER=34"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=true"
+export "PACKAGE_CONFIG=/Volumes/Glyph2TB/Users/chris/workspace/background-geolocation/flutter/flutter_background_geolocation/example/.dart_tool/package_config.json"
---
+++ flutter_ba:Users:seonghunbyeon:Downloads:flutter_background_geolocation-4.3.1ckground_geolocation-4.3.1/example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,42 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<background_fetch/BackgroundFetchPlugin.h>)
+#import <background_fetch/BackgroundFetchPlugin.h>
+#else
+@import background_fetch;
+#endif
+
+#if __has_include(<flutter_background_geolocation/FLTBackgroundGeolocationPlugin.h>)
+#import <flutter_background_geolocation/FLTBackgroundGeolocationPlugin.h>
+#else
+@import flutter_background_geolocation;
+#endif
+
+#if __has_include(<shared_preferences_ios/FLTSharedPreferencesPlugin.h>)
+#import <shared_preferences_ios/FLTSharedPreferencesPlugin.h>
+#else
+@import shared_preferences_ios;
+#endif
+
+#if __has_include(<url_launcher/FLTURLLauncherPlugin.h>)
+#import <url_launcher/FLTURLLauncherPlugin.h>
+#else
+@import url_launcher;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [BackgroundFetchPlugin registerWithRegistrar:[registry registrarForPlugin:@"BackgroundFetchPlugin"]];
+  [FLTBackgroundGeolocationPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTBackgroundGeolocationPlugin"]];
+  [FLTSharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharedPreferencesPlugin"]];
+  [FLTURLLauncherPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTURLLauncherPlugin"]];
+}
+
+@end
